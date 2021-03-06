1822fd79d82db7913f7523f44514e2932cc40b70
lihh <lihuanhuan1003@hotmail.com>
2021-03-05 17:36:15 -0800

    Add function specs

---
+++ R/plot_hashtags.R
@@ -0,0 +1,15 @@
+#' Plot hashtag word analysis
+#'
+#' A chart plotting analysis result of the most commonly used 
+#' words in tweets.
+#' 
+#' @param df A dataframe
+#' @param text_col A column name in dataframe
+#'
+#' @return A chart plotting analysis the most commonly used words.
+#'
+#' @examples (tweet_data, time)
+#' 
+plot_hashtags <- function(df, text_col){
+    
+}
---
+++ R/plot_timeline.R
@@ -0,0 +1,15 @@
+#' Plot timeline analysis
+#'
+#' Analysis what time of day the tweets occurs and plot the
+#' counts of tweets versus hours. 
+#' 
+#' @param df A dataframe
+#' @param time_col A column name in dataframe
+#'
+#' @return A chart plotting the counts of tweets versus hours.
+#'
+#' @examples (tweet_data, time)
+#' 
+plot_timeline <- function(df, time_col){
+    
+}
