c6b6e56e68f6214a8271a9f41e39dc08881cf375
Jared Splinter <JaredSplint@gmail.com>
2021-03-06 15:28:05 -0800

    add function documentation for visualize_sentiments

---
+++ R/tweetr.R
@@ -0,0 +1,15 @@
+#' Visualize Sentiment Analysis
+#'
+#' Takes in the output of sentiment_analysis and creates
+#' a visualization of user's tweets with sentimental analysis.
+#'
+#' @param sentiment_df A DataFrame containing sentiment column from sentiment_analysis
+#' @param plot_type An optional string for which chart to return. Options are ("Standard", "Stacked", "Separate")
+#'
+#' @return A bar chart containing most common words in tweets, colour coded by sentiment.
+#' @export
+#'
+#' @examples visualize_sentiments(sentiment_df, plot_type = "Stacked")
+visualize_sentiments <- function(sentiment_df, plot_type = "Standard") {
+
+}
